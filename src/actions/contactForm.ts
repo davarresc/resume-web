531ce39dc46a2fd8cf03d2fd278abba9af30ba35
David <arroyoescobardavid@gmail.com>
2025-11-08 13:42:46 -0600

    feat: send emails with nodemail temporarily

--- src/actions/contactForm.ts
+++ src/actions/contactForm.ts
@@ -1,5 +1,6 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
+import nodemailer from "nodemailer";
 
 const contactSchema = z.object({
   firstName: z.string().min(2),
@@ -8,10 +9,34 @@ const contactSchema = z.object({
   message: z.string(),
 });
 
+type ContactSchema = z.infer<typeof contactSchema>;
+
 export default defineAction({
   accept: "form",
   input: contactSchema,
   handler: async (input) => {
-    return `Hello, ${input.firstName}!`;
+    await sendMail(input);
+
+    return { success: true };
   },
 });
+
+// TODO Update to the resume notifiication mimcroservice when available
+const sendMail = async (input: ContactSchema) => {
+  const transporter = nodemailer.createTransport({
+    host: import.meta.env.SMTP_HOST,
+    port: import.meta.env.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: import.meta.env.SMTP_USER,
+      pass: import.meta.env.SMTP_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"${input.firstName} ${input.lastName}" <${input.email}>`,
+    to: "emaildavidprueba@gmail.com",
+    subject: "Resume web: Nuevo mensaje disponible",
+    text: input.message,
+  });
+};
