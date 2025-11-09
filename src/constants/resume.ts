425eafc18fb732fb2608c7dbafe519e21315eca6
David <arroyoescobardavid@gmail.com>
2025-11-08 22:13:25 -0600

    feat: added resume page

---
+++ src/constants/resume.ts
@@ -0,0 +1,253 @@
+export const experience = {
+  en: [
+    {
+      startDate: "02/2022",
+      endDate: "08/2025",
+      location: "Madrid, Spain",
+      company: "Accedo.tv",
+      position: "Senior Software Engineer",
+      description: `<div class="flex flex-col">
+    Development of multiple applications for a subscription-based video
+    streaming service tailored for CTV platforms.
+    <ul class="list-disc ms-10">
+      <li>
+        Built a Roku application from scratch using BrightScript, covering the
+        entire development lifecycle.
+      </li>
+      <li>
+        Refactored and migrated a Lightning project to TypeScript, improving
+        code quality, performance and maintainability while implementing new
+        features.
+      </li>
+      <li>Maintained and implemented new features in a React application.</li>
+    </ul>
+  </div>
+`,
+    },
+    {
+      startDate: "10/2019",
+      endDate: "02/2022",
+      location: "Seville, Spain",
+      company: "Sngular",
+      position: "Senior Frontend Developer",
+      description: `<div class="flex flex-col">
+    <p>Development of frontend application using react.</p>
+    <p>Development and maintenance of a Web Component catalog with LitElement.</p>
+  </div>
+`,
+    },
+    {
+      startDate: "2019",
+      location: "Seville, Spain",
+      company: "Freelance",
+      position: "Full-stack Software Engineer",
+      description: `<div class="flex flex-col">
+   Development of full-stack web applications using Typescript with Ember.js and express.js
+  </div>
+`,
+    },
+    {
+      startDate: "05/2017",
+      endDate: "10/2019",
+      location: "Sevilla, Spain",
+      company: "Ximdex",
+      position: "Senior developer",
+      description: `<div class="flex flex-col">
+    <p>Full-stack development of headless applications and CMS in PHP with MYSQL, Solr and MongoDB.</p>
+    <p>Frontend development with Angular/React and integration of APIs and distributed data sources using Python (web scraping, ElasticSearch, Flask, Celery).</p>
+  </div>
+`,
+    },
+    {
+      startDate: "10/2016",
+      endDate: "05/2017",
+      location: "Sevilla, Spain",
+      company: "Natural Pixel",
+      position: "Web developer",
+      description: `<div class="flex flex-col">
+   Development and maintenance of CRM systems and websites using Yii2, Wordpress and Prestashop.
+  </div>
+`,
+    },
+    {
+      startDate: "4/2016",
+      endDate: "10/2016",
+      location: "Sevilla, Spain",
+      company: "Google Summer of Code 2016",
+      position: "Programmer",
+      description: `<div class="flex flex-col">
+   Create a repository connector and an authority connector for Nuxeo using Apache ManifoldCF.
+  </div>
+`,
+    },
+    {
+      startDate: "05/2015",
+      endDate: "04/2016",
+      location: "Sevilla, Spain",
+      company: "Ingae",
+      position: "Software developer",
+      description: `<div class="flex flex-col">
+Development of a website, a CRM and an application focus on energy efficiency.
+  </div>
+`,
+    },
+  ],
+  es: [
+    {
+      startDate: "02/2022",
+      endDate: "08/2025",
+      location: "Madrid, España",
+      company: "Accedo.tv",
+      position: "Senior Software Engineer",
+      description: `<div class="flex flex-col">
+    Development of multiple applications for a subscription-based video
+    streaming service tailored for CTV platforms.
+    <ul class="list-disc ms-10">
+      <li>
+        Built a Roku application from scratch using BrightScript, covering the
+        entire development lifecycle.
+      </li>
+      <li>
+        Refactored and migrated a Lightning project to TypeScript, improving
+        code quality, performance and maintainability while implementing new
+        features.
+      </li>
+      <li>Maintained and implemented new features in a React application.</li>
+    </ul>
+  </div>
+`,
+    },
+    {
+      startDate: "10/2019",
+      endDate: "02/2022",
+      location: "Seville, España",
+      company: "Sngular",
+      position: "Senior Frontend Developer",
+      description: `<div class="flex flex-col">
+    <p>Desarrollo de aplicaciones frontend con react.</p>
+    <p>Desarrollo y mantenimiento de un catálogo de componentes con LitElement.</p>
+  </div>
+`,
+    },
+    {
+      startDate: "2019",
+      location: "Sevilla, España",
+      company: "Freelance",
+      position: "Ingeniero de Software Full-stack",
+      description: `<div class="flex flex-col">
+   Development of full-stack web applications using Typescript with Ember.js and express.js
+  </div>
+`,
+    },
+    {
+      startDate: "05/2017",
+      endDate: "10/2019",
+      location: "Sevilla, España",
+      company: "Ximdex",
+      position: "Desarrollador senior",
+      description: `<div class="flex flex-col">
+    <p>Full-stack development of headless applications and CMS in PHP with MYSQL, Solr and MongoDB.</p>
+    <p>Frontend development with Angular/React and integration of APIs and distributed data sources using Python (web scraping, ElasticSearch, Flask, Celery).</p>
+  </div>
+`,
+    },
+    {
+      startDate: "10/2016",
+      endDate: "05/2017",
+      location: "Sevilla, España",
+      company: "Natural Pixel",
+      position: "Desarrolldor web",
+      description: `<div class="flex flex-col">
+   Development and maintenance of CRM systems and websites using Yii2, Wordpress and Prestashop.
+  </div>
+`,
+    },
+    {
+      startDate: "4/2016",
+      endDate: "10/2016",
+      location: "Sevilla, España",
+      company: "Google Summer of Code 2016",
+      position: "Programador",
+      description: `<div class="flex flex-col">
+   Create a repository connector and an authority connector for Nuxeo using Apache ManifoldCF.
+  </div>
+`,
+    },
+    {
+      startDate: "05/2015",
+      endDate: "04/2016",
+      location: "Sevilla, Spain",
+      company: "Ingae",
+      position: "Desarrollador de software",
+      description: `<div class="flex flex-col">
+Development of a website, a CRM and an application focus on energy efficiency.
+  </div>
+`,
+    },
+  ],
+};
+
+export const education = {
+  en: [
+    {
+      startDate: "02/2022",
+      endDate: "08/2025",
+      location: "Seville, Spain",
+      place: "Universidad de Sevilla",
+      title: "Bachelor's Degree in Software Engineering, Computer Science",
+    },
+    {
+      startDate: "10/2019",
+      endDate: "02/2022",
+      location: "Seville, Spain",
+      place: "I.E.S Nervion",
+      title: "Higher Technician in development of Multiplatform Applications",
+    },
+  ],
+  es: [
+    {
+      startDate: "02/2022",
+      endDate: "08/2025",
+      location: "Sevilla, España",
+      place: "Universidad de Sevilla",
+      title: "Grado en Ingeniería informática de Software",
+    },
+    {
+      startDate: "10/2019",
+      endDate: "02/2022",
+      location: "Sevilla, España",
+      place: "I.E.S Nervion",
+      title:
+        "CFGS Técnico Superior en Desarrollo de Aplicaciones Multiplatforma",
+    },
+  ],
+};
+
+export const certifications = {
+  en: [
+    {
+      title: "Neo4j Certified Professional",
+      id: "16849020",
+      issued: "Jul 2019",
+    },
+    {
+      title: "JSNAD: OpenJS Node.js Application Developer",
+      id: "LF-saf3xplo01",
+      issued: "Dec 2021",
+      expired: "Dec 2024",
+    },
+  ],
+  es: [
+    {
+      title: "Neo4j Certified Professional",
+      id: "16849020",
+      issued: "Jul 2019",
+    },
+    {
+      title: "JSNAD: OpenJS Node.js Application Developer",
+      id: "LF-saf3xplo01",
+      issued: "Dic 2021",
+      expired: "Dic 2024",
+    },
+  ],
+};
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,7 @@
 // Route slugs for each locale
 export const ROUTES = {
   home: { en: "", es: "" },
-  resume: { en: "Resume", es: "Curriculum" },
+  resume: { en: "resume", es: "curriculum" },
   projects: { en: "projects", es: "proyectos" },
   contact: { en: "contact", es: "contacto" },
 } as const;
