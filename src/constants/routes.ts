bc29b38fece150dcdbffc95fc0e5ecc3294c6052
David <arroyoescobardavid@gmail.com>
2025-11-09 09:36:35 -0600

    feat: added changelog page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,6 +4,12 @@ export const ROUTES = {
   resume: { en: "resume", es: "curriculum" },
   projects: { en: "projects", es: "proyectos" },
   contact: { en: "contact", es: "contacto" },
+  changelog: { en: "changelog", es: "changelog" },
 } as const;
 
 export type RouteKey = keyof typeof ROUTES;
+
+const excludeRoutes = ["changelog"];
+export const MENU = Object.fromEntries(
+  Object.entries(ROUTES).filter(([key]) => !excludeRoutes.includes(key)),
+);
